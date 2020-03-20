289f59b0bed4ef054440958fecf596d024371652
Personne <beroule.alexandre@gmail.com>
2020-03-20 14:49:02 +0100

    Quizz Fonctionnel Merci Robin

--- app/src/main/java/com/beroule/projet_flashcard/EasyActivity.java
+++ app/src/main/java/com/beroule/projet_flashcard/EasyActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.media.MediaPlayer;
-import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -13,15 +12,12 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-
-import javax.security.auth.login.LoginException;
-
 public class EasyActivity extends AppCompatActivity {
 
     int CurrentlyQuestion;
     int CurrentlyQuestionTotal;
     private Champion currentChampion;
+    int index = 0;
 
     static int[] tableauSon = {R.raw.aatrox_selection, R.raw.rammus_selection, R.raw.rengar_selection, R.raw.caitlyn_selection, R.raw.garen_selection,
     R.raw.ezreal_selection, R.raw.alistar_selection, R.raw.katarina_selection, R.raw.drmundo_selection, R.raw.mordekaiser_selection};
@@ -33,12 +29,14 @@ public class EasyActivity extends AppCompatActivity {
         setContentView(R.layout.activity_easy);
 
         QuestionGenerator.load();
-        currentChampion = QuestionGenerator.tableauChampion.get(0);
+        currentChampion = QuestionGenerator.tableauChampion.get(index);
 
-        RadioButton radioButton1 = findViewById(R.id.taric);
+        RadioButton radioButton1 = findViewById(R.id.answer1RadioButton);
         radioButton1.setText(currentChampion.answers.get(0));
-        RadioButton radioButton2 = findViewById(R.id.aatrox);
+        RadioButton radioButton2 = findViewById(R.id.answer2RadioButton);
         radioButton2.setText(currentChampion.answers.get(1));
+        RadioButton radioButton3 = findViewById(R.id.answer3RadioButton);
+        radioButton3.setText(currentChampion.answers.get(2));
 
         CurrentlyQuestionTotal = 10;
         CurrentlyQuestion = 1;
@@ -86,6 +84,16 @@ public class EasyActivity extends AppCompatActivity {
                 }
                 else{
                     CurrentlyQuestion ++;
+                    index++;
+
+                    currentChampion = QuestionGenerator.tableauChampion.get(index);
+                    RadioButton radioButton1 = findViewById(R.id.answer1RadioButton);
+                    radioButton1.setText(currentChampion.answers.get(0));
+                    RadioButton radioButton2 = findViewById(R.id.answer2RadioButton);
+                    radioButton2.setText(currentChampion.answers.get(1));
+                    RadioButton radioButton3 = findViewById(R.id.answer3RadioButton);
+                    radioButton3.setText(currentChampion.answers.get(2));
+
                     Log.i("EasyActivity", String.valueOf(totalSon));
 
                     TextView currentlyQuestionTextView = findViewById(R.id.currentlyQuestionTextView);
